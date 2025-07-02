e083cc4e53b0cf91370fb4bb076e379a37acbd2a
Alex Petrychuk <93341599+petalser@users.noreply.github.com>
2025-07-02 20:33:26 +0200

    Updated type definition

--- index.d.ts
+++ index.d.ts
@@ -1,45 +1,31 @@
-
-// declare class Logger {
-//     /**
-//      * Logs a message with a timestamp and a flag.
-//      * @param message - The log message.
-//      * @param flag - A label for the log type. Defaults to 'INFO'.
-//      * @param meta - Optional metadata object with extra info.
-//      */
-//     static info(message: string, flag?: string, meta?: Record<string, any>): void;
-// }
-
-// export = Logger;
-
 interface LoggerMeta {
     [key: string]: any;
 }
 
 /**
-//  * Logger class for standardized logging.
-//  * 
-//  * @class
-//  * 
-//  * @method initialize
-//  * @method info
-//  */
+  * Logger class for standardized logging.
+  * @class
+  * @method initialize
+  * @method info
+  */
 declare class Logger {
     /**
      * Initialize logger with folder path for logs (optional).
      * If not provided, logs only to console
+     * E.g. Logger.initialize('./logs')
      * @param {string} [logFolder] - Enables file logging if provided.
      */
     static initialize(logFolder?: string): void;
     /**
     * Logs provided message to console
-    * If Logger initialized, updates "info-xxx" file
+    * If Logger initialized, updates "./your-logs-path/info/info-xxx" file
     * @param {string} message - Enables file logging if provided.
     * @param {LoggerMeta} [meta] - Optional object with your information.
     */
     static info(message: string, meta?: LoggerMeta): void;
     /**
     * Logs provided message to console
-    * If Logger initialized, updates "error-xxx" file
+    * If Logger initialized, updates "./your-logs-path/info/error-xxx" file
     * @param {string} message - Enables file logging if provided.
     * @param {LoggerMeta} [meta] - Optional object with your information.
     */
